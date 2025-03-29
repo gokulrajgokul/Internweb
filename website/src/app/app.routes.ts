69429df42c4eac9e98671acf97a49f1860cd28f1
gokulrajgokul <gokul26.cbcs@gmail.com>
2025-03-29 12:02:43 +0530

    task pages added

--- website/src/app/app.routes.ts
+++ website/src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { RegisterinternComponent } from './registerintern/registerintern.component';
 import { AssigntaskComponent } from './assigntask/assigntask.component';
 import { InterndetailsComponent } from './interndetails/interndetails.component';
+import { PendingtaskComponent } from './pendingtask/pendingtask.component';
+import { TaskreportComponent } from './taskreport/taskreport.component';
 
 export const routes: Routes = [
     {path:'',component:RegisterinternComponent},
     {path:'registerintern',component:RegisterinternComponent},
     {path:'interndetails',component:InterndetailsComponent},
-    {path:'assigntask',component:AssigntaskComponent}
+    {path:'assigntask',component:AssigntaskComponent},
+    {path:'pendingtask',component:PendingtaskComponent},
+    {path:'taskreport',component:TaskreportComponent}
 ];
---
+++ website/src/app/pendingtask/pendingtask.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pendingtask',
+  imports: [],
+  templateUrl: './pendingtask.component.html',
+  styleUrl: './pendingtask.component.css'
+})
+export class PendingtaskComponent {
+
+}
