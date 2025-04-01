930ba0e1cc7d50fc7066c8aed65502dd2eadf3c7
Saran <saranvms2002@gmail.com>
2025-04-01 11:30:40 +0530

    toggle

--- website/src/app/registerintern/registerintern.component.ts
+++ website/src/app/registerintern/registerintern.component.ts
@@ -1,20 +1,4 @@
-// import { Component } from '@angular/core';
-
-// @Component({
-//   selector: 'app-registerintern',
-//   imports: [],
-//   templateUrl: './registerintern.component.html',
-//   styleUrl: './registerintern.component.css'
-// })
-// export class RegisterinternComponent {
-
-// }
-
-
-// togge button
-
-import { Component, AfterViewInit } from '@angular/core';
-declare var bootstrap: any;
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-registerintern',
@@ -22,14 +6,6 @@ declare var bootstrap: any;
   templateUrl: './registerintern.component.html',
   styleUrl: './registerintern.component.css'
 })
-export class RegisterinternComponent implements AfterViewInit {
-  ngAfterViewInit() {
-    const navbarToggler = document.querySelector('.navbar-toggler');
-    navbarToggler?.addEventListener('click', () => {
-      const bsCollapse = new bootstrap.Collapse(document.getElementById('navbarNav'), {
-        toggle: true
-      });
-    });
-  }
-}
+export class RegisterinternComponent {
 
+}
