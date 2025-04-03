48876586d596c3222b609bff7bbc0ebe45637ffe
gokulrajgokul <gokul26.cbcs@gmail.com>
2025-04-03 10:59:42 +0530

    changesss

--- djangointern/interns/models.py
+++ djangointern/interns/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+
+class registerintern(models.Model):
+    internname=models.CharField(max_length = 255,unique=True)
+    password=models.CharField(max_length = 255)
+    mail=models.EmailField(unique=True)
+    domain=models.CharField(max_length = 255)
+    duration=models.IntegerField(null=False)
+    date=models.DateField()
+    whatsapp=models.IntegerField()
+    collegename=models.CharField(max_length = 255)
+    passyear=models.IntegerField(null=False)
+    degree=models.CharField(max_length = 255)
