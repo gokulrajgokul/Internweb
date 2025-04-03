23ae2a7082c455a878e49b02c310f986dffef2b1
gokulrajgokul <gokul26.cbcs@gmail.com>
2025-04-03 12:27:37 +0530

    footer created

--- website/src/app/app.routes.ts
+++ website/src/app/app.routes.ts
@@ -4,6 +4,9 @@ import { AssigntaskComponent } from './assigntask/assigntask.component';
 import { InterndetailsComponent } from './interndetails/interndetails.component';
 import { PendingtaskComponent } from './pendingtask/pendingtask.component';
 import { TaskreportComponent } from './taskreport/taskreport.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     {path:'',component:RegisterinternComponent},
@@ -11,5 +14,8 @@ export const routes: Routes = [
     {path:'interndetails',component:InterndetailsComponent},
     {path:'assigntask',component:AssigntaskComponent},
     {path:'pendingtask',component:PendingtaskComponent},
-    {path:'taskreport',component:TaskreportComponent}
+    {path:'taskreport',component:TaskreportComponent},
+    {path:'header',component:HeaderComponent},
+    {path:'footer',component:FooterComponent},
+    {path:'home',component:HomeComponent}
 ];
