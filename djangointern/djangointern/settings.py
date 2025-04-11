811b361b15398f43a42458b06401752d9ec0b35b
Saran <saranvms2002@gmail.com>
2025-04-11 12:16:14 +0530

    api

--- djangointern/djangointern/settings.py
+++ djangointern/djangointern/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'interns.apps.InternsConfig',
+    'djangointern',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- djangointern/interns/views.py
+++ djangointern/interns/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
-
-# Create your views here.
-from rest_framework.views import APIView
-
+from django.shortcuts import render, redirect
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from .models import models
 
 class HelloWorldView(APIView):
+    def post(self,request):
+        print(request.data)
+        return Response('save')
+    
     def get(self, request):
         return Response({"message": "Hello, world!"}, status=status.HTTP_200_OK)
