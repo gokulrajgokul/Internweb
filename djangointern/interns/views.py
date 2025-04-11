10b1a16cba6637fa63f974393cbd8e21b342634d
Saran <saranvms2002@gmail.com>
2025-04-11 10:51:56 +0530

    api

--- djangointern/interns/views.py
+++ djangointern/interns/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 from rest_framework.views import APIView
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+class HelloWorldView(APIView):
+    def get(self, request):
+        return Response({"message": "Hello, world!"}, status=status.HTTP_200_OK)
