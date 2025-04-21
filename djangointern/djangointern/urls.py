3ee81de593c2fca8ca70788928f93f07e5ca6b0c
Saran <saranvms2002@gmail.com>
2025-04-21 11:05:41 +0530

    dj

--- djangointern/djangointern/urls.py
+++ djangointern/djangointern/urls.py
@@ -1,9 +1,9 @@
-from django.contrib import admin
-from django.urls import path
+# from django.contrib import admin
+# from django.urls import path
 from interns.views import HelloWorldView
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('hello/', HelloWorldView.as_view()),
-]
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('hello/', HelloWorldView.as_view()),
+# ]
 
